9db03fa7aef88b78cf4a0b0c470294a69b86570d
Thibault GAUTHIER <gauthierthibault@live.fr>
2014-02-26 17:36:59 +0100

    Problème persistant au niveau de la création de l'arbre.

--- src/branchAndBound/AlgorithmeBranchAndBound.java
+++ src/branchAndBound/AlgorithmeBranchAndBound.java
@@ -10,47 +10,53 @@ public class AlgorithmeBranchAndBound {
 
 	private Model model;
 	private SacADos sacADos;
-	
+
 	public AlgorithmeBranchAndBound(Model model) {
 		this.model = model;
 		sacADos = new SacADos();
 		SacADos sacResultat = rechercheOptimale(sacADos, 0);
 	}
-	
+
 	public SacADos rechercheOptimale(SacADos sacADos, int increment) {
 		
-		String nomEntreprise = model.getEntreprises().get(increment);
-		
-		// Pour chaque base contenant l'entreprise 
-		for (Base base : model.getBases()) {
+		if (increment < model.getEntreprises().size()) {
 			
-			System.out.println("ENTREPRISE -> " + nomEntreprise);
-			System.out.println("BASE -> " + base.getNomBase());
-			
-			if (base.getEntreprises().contains(nomEntreprise)) {
-				// et qui n'est pas dans la liste partielle
-				if (!sacADos.getResultatsPartiels().containsKey(base.getNomBase())) {
-					System.out.println("JE PASSE ICI");
-					SacADos nouveauSac = new SacADos();
-					nouveauSac.setResultatsPartiels((HashMap<String, ArrayList<String>>)sacADos.getResultatsPartiels().clone());
-					ArrayList<String> nouvelleListeEntreprises = new ArrayList<String>();
-					nouvelleListeEntreprises.add(nomEntreprise);
-					nouveauSac.ajouterEntree(base.getNomBase(), nouvelleListeEntreprises);
-					nouveauSac.setCoutOptimal(sacADos.getCoutOptimal() + base.getCoutBase());
-					nouveauSac.afficher();
-				} else {
-					System.out.println("OU JE PASSE LA");
-					SacADos nouveauSac = new SacADos();
-					nouveauSac.setResultatsPartiels((HashMap<String, ArrayList<String>>)sacADos.getResultatsPartiels().clone());
-					nouveauSac.ajouterEntreprise(base.getNomBase(), nomEntreprise);
-					nouveauSac.afficher();
+			String nomEntreprise = model.getEntreprises().get(increment);
+
+			// Pour chaque base contenant l'entreprise 
+			for (Base base : model.getBases()) {
+
+				System.out.println("INCREMENT -> " + increment);
+				System.out.println("ENTREPRISE -> " + nomEntreprise);
+				System.out.println("BASE -> " + base.getNomBase());
+
+				if (base.getEntreprises().contains(nomEntreprise)) {
+					// et qui n'est pas dans la liste partielle
+					if (!sacADos.getResultatsPartiels().containsKey(base.getNomBase())) {
+						System.out.println("JE PASSE ICI");
+						SacADos nouveauSac = new SacADos();
+						nouveauSac.setResultatsPartiels((HashMap<String, ArrayList<String>>)sacADos.getResultatsPartiels().clone());
+						ArrayList<String> nouvelleListeEntreprises = new ArrayList<String>();
+						nouvelleListeEntreprises.add(nomEntreprise);
+						nouveauSac.ajouterEntree(base.getNomBase(), nouvelleListeEntreprises);
+						nouveauSac.setCoutOptimal(sacADos.getCoutOptimal() + base.getCoutBase());
+						nouveauSac.afficher();
+						rechercheOptimale(nouveauSac, increment + 1);
+
+					} else {
+						System.out.println("OU JE PASSE LA");
+						SacADos nouveauSac = new SacADos();
+						nouveauSac.setResultatsPartiels((HashMap<String, ArrayList<String>>)sacADos.getResultatsPartiels().clone());
+						nouveauSac.ajouterEntreprise(base.getNomBase(), nomEntreprise);
+						nouveauSac.setCoutOptimal(sacADos.getCoutOptimal());
+						nouveauSac.afficher();
+						rechercheOptimale(nouveauSac, increment + 1);
+					}
 				}
+				System.out.println("\n\n");
 			}
 		}
-		
-		
-		
-		
+
 		return null;
 	}
 
@@ -69,5 +75,5 @@ public class AlgorithmeBranchAndBound {
 	public void setSacADos(SacADos sacADos) {
 		this.sacADos = sacADos;
 	}
-	
+
 }
--- src/branchAndBound/SacADos.java
+++ src/branchAndBound/SacADos.java
@@ -2,6 +2,7 @@ package branchAndBound;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map.Entry;
 
 public class SacADos {
 	
@@ -14,13 +15,29 @@ public class SacADos {
 	}
 
 	public void ajouterEntree(String base, ArrayList<String> entreprises) {
-		resultatsPartiels.put(base, entreprises);
+		boolean estPresent = false;
+		for (Entry<String, ArrayList<String>> entry : resultatsPartiels.entrySet()) {
+			if (entry.getValue().contains(entreprises.get(0))) {
+				estPresent = true;
+			}
+		}
+		if (!estPresent) {
+			resultatsPartiels.put(base, entreprises);	
+		}
 	}
 	
 	public void ajouterEntreprise(String base, String entreprise) {
 		ArrayList<String> temporaire = resultatsPartiels.get(base);
-		temporaire.add(entreprise);
-		resultatsPartiels.put(base, temporaire);
+		boolean estPresent = false;
+		for (Entry<String, ArrayList<String>> entry : resultatsPartiels.entrySet()) {
+			if (entry.getValue().contains(entreprise)) {
+				estPresent = true;
+			}
+		}
+		if (!estPresent) {
+			temporaire.add(entreprise);
+			resultatsPartiels.put(base, temporaire);	
+		}
 	}
 	
 	public HashMap<String, ArrayList<String>> getResultatsPartiels() {
@@ -42,7 +59,6 @@ public class SacADos {
 	public void afficher() {
 		System.out.println(resultatsPartiels.toString());
 		System.out.println(coutOptimal);
-		System.out.println("\n\n");
 	}
 	
 }
