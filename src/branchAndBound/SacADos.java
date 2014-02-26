60d72586722407cace1e2eff46902f651f37099e
Thibault GAUTHIER <gauthierthibault@live.fr>
2014-02-26 15:05:07 +0100

    Structure générale.

--- src/branchAndBound/SacADos.java
+++ src/branchAndBound/SacADos.java
@@ -1,9 +1,31 @@
 package branchAndBound;
 
+import java.util.ArrayList;
+
 public class SacADos {
 	
+	private ArrayList<String> listeResultats;
+	private int coutOptimal;
+	
 	public SacADos() {
-		
+		this.listeResultats = new ArrayList<String>();
+		this.coutOptimal = 0;
+	}
+
+	public ArrayList<String> getListeResultats() {
+		return listeResultats;
+	}
+
+	public void setListeResultats(ArrayList<String> listeResultats) {
+		this.listeResultats = listeResultats;
+	}
+
+	public int getCoutOptimal() {
+		return coutOptimal;
+	}
+
+	public void setCoutOptimal(int coutOptimal) {
+		this.coutOptimal = coutOptimal;
 	}
 
 }
