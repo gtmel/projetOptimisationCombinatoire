97aa54f015c95eee5e4a7e704aabf3e47dd2b9fa
geeknbar <dorian.coffinet@gmail.com>
2014-02-28 15:23:11 +0100

    Tests sur les vraies listes.

--- src/ihm/Main.java
+++ src/ihm/Main.java
@@ -7,8 +7,10 @@ import readers.Lecteur;
 public class Main {
 
 	public static void main(String[] args) {
-		String cheminListeBases = "./bin/docs/listesBases/lb1.txt";
-		String cheminListeEntreprises = "./bin/docs/listesEntreprises/le1.txt";
+//		String cheminListeBases = "./bin/docs/listesBases/lb1.txt";
+		String cheminListeBases = "./bin/docs/listesBases/ListeBases3.txt";
+//		String cheminListeEntreprises = "./bin/docs/listesEntreprises/le1.txt";
+		String cheminListeEntreprises = "./bin/docs/listesEntreprises/ListeEntreprises1.txt";
 		Lecteur lecteur = new Lecteur(cheminListeBases, cheminListeEntreprises);
 		Model model = new Model();
 		model.setBases(lecteur.lireBases());
