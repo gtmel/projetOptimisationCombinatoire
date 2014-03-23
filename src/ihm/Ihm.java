acb16b34b61dc1f9d7f635bd1dbfdadc3c97a0b8
Thibault GAUTHIER <thibaultgauthier.etud@gmail.com>
2014-03-23 17:10:47 +0100

    Début du transfert vers l'IHM (le tsf de l'algo glouton est opérationnel).

--- src/ihm/Ihm.java
+++ src/ihm/Ihm.java
@@ -1,9 +1,15 @@
 package ihm;
 
+import glouton.AlgorithmeGlouton;
+
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+
 import java.awt.BorderLayout;
+
+import javax.swing.ButtonGroup;
+import javax.swing.DefaultListModel;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
 import javax.swing.JTextArea;
@@ -11,15 +17,34 @@ import javax.swing.BoxLayout;
 import javax.swing.Box;
 import javax.swing.JButton;
 import javax.swing.SwingConstants;
+
 import java.awt.Component;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Map.Entry;
+
 import javax.swing.JList;
 import javax.swing.JRadioButton;
-import javax.swing.AbstractListModel;
+import javax.swing.border.TitledBorder;
+
+import branchAndBound.AlgorithmeBranchAndBound;
+import model.Model;
+import readers.Lecteur;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Ihm {
 
 	private JFrame frame;
-
+	private JList<String> listeEntreprises;
+	private JList<String> listeBases;
+	private JRadioButton rdbtnBranchBound;
+	private JRadioButton rdbtnGlouton;
+	private ButtonGroup choixAlgorithme;
+	private JTextArea textAreaResultats;
+	
 	/**
 	 * Launch the application.
 	 */
@@ -62,72 +87,15 @@ public class Ihm {
 		JLabel lblResultats = new JLabel("Résultats");
 		verticalBox.add(lblResultats);
 		
-		JTextArea textAreaResultats = new JTextArea();
+		textAreaResultats = new JTextArea();
 		verticalBox.add(textAreaResultats);
 		
 		JPanel panelOptions = new JPanel();
 		frame.getContentPane().add(panelOptions, BorderLayout.WEST);
 		panelOptions.setLayout(new BorderLayout(0, 0));
 		
-		JPanel panelChoixListes = new JPanel();
-		panelOptions.add(panelChoixListes, BorderLayout.NORTH);
-		panelChoixListes.setLayout(new BoxLayout(panelChoixListes, BoxLayout.X_AXIS));
-		
-		Box verticalBoxEB = Box.createVerticalBox();
-		panelChoixListes.add(verticalBoxEB);
-		
-		Box horizontalBoxLblEB = Box.createHorizontalBox();
-		verticalBoxEB.add(horizontalBoxLblEB);
-		
-		Component horizontalStrutEBLbl1 = Box.createHorizontalStrut(20);
-		horizontalBoxLblEB.add(horizontalStrutEBLbl1);
-		
-		JLabel lblEntreprisesBases = new JLabel("Choix Entreprises/Bases");
-		horizontalBoxLblEB.add(lblEntreprisesBases);
-		
-		Component horizontalStrutEBLbl2 = Box.createHorizontalStrut(20);
-		horizontalBoxLblEB.add(horizontalStrutEBLbl2);
-		
-		Component verticalStrutEB = Box.createVerticalStrut(20);
-		verticalBoxEB.add(verticalStrutEB);
-		
-		Box horizontalBoxJlistEB = Box.createHorizontalBox();
-		verticalBoxEB.add(horizontalBoxJlistEB);
-		
-		Component horizontalStrutJEB1 = Box.createHorizontalStrut(20);
-		horizontalBoxJlistEB.add(horizontalStrutJEB1);
-		
-		JList listeEntreprises = new JList();
-		listeEntreprises.setModel(new AbstractListModel() {
-			String[] values = new String[] {"Liste E 1", "Liste E 2", "Liste E 3", "Liste E 4"};
-			public int getSize() {
-				return values.length;
-			}
-			public Object getElementAt(int index) {
-				return values[index];
-			}
-		});
-		horizontalBoxJlistEB.add(listeEntreprises);
-		
-		Component horizontalGlueJEB1 = Box.createHorizontalGlue();
-		horizontalBoxJlistEB.add(horizontalGlueJEB1);
-		
-		JList listeBases = new JList();
-		listeBases.setModel(new AbstractListModel() {
-			String[] values = new String[] {"Liste B 1", "Liste B 2", "Liste B 3", "Liste B 4"};
-			public int getSize() {
-				return values.length;
-			}
-			public Object getElementAt(int index) {
-				return values[index];
-			}
-		});
-		horizontalBoxJlistEB.add(listeBases);
-		
-		Component horizontalStrutJEB2 = Box.createHorizontalStrut(20);
-		horizontalBoxJlistEB.add(horizontalStrutJEB2);
-		
 		JPanel panelAlgorithmes = new JPanel();
+		panelAlgorithmes.setBorder(new TitledBorder(null, "Algorithmes", TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		panelOptions.add(panelAlgorithmes, BorderLayout.SOUTH);
 		panelAlgorithmes.setLayout(new BoxLayout(panelAlgorithmes, BoxLayout.X_AXIS));
 		
@@ -137,20 +105,41 @@ public class Ihm {
 		Box horizontalBoxTitre = Box.createHorizontalBox();
 		verticalBoxPanelOptions.add(horizontalBoxTitre);
 		
-		JLabel lblAlgorithmes = new JLabel("Algorithmes");
-		horizontalBoxTitre.add(lblAlgorithmes);
-		
 		Box horizontalBoxOptions = Box.createHorizontalBox();
 		verticalBoxPanelOptions.add(horizontalBoxOptions);
 		
 		Box verticalBoxChoix = Box.createVerticalBox();
 		horizontalBoxOptions.add(verticalBoxChoix);
 		
-		JRadioButton rdbtnBranchBound = new JRadioButton("Branch & Bound");
-		verticalBoxChoix.add(rdbtnBranchBound);
+		choixAlgorithme = new ButtonGroup();
 		
-		JRadioButton rdbtnGlouton = new JRadioButton("Glouton");
+		rdbtnBranchBound = new JRadioButton("Branch & Bound");
+		verticalBoxChoix.add(rdbtnBranchBound);
+		choixAlgorithme.add(rdbtnBranchBound);
+		
+		rdbtnGlouton = new JRadioButton("Glouton");
 		verticalBoxChoix.add(rdbtnGlouton);
+		choixAlgorithme.add(rdbtnGlouton);
+		
+		JPanel panelEntreprisesBases = new JPanel();
+		panelOptions.add(panelEntreprisesBases, BorderLayout.NORTH);
+		panelEntreprisesBases.setLayout(new BorderLayout(0, 0));
+		
+		JPanel panelEntreprises = new JPanel();
+		panelEntreprises.setBorder(new TitledBorder(null, "Entreprises", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panelEntreprisesBases.add(panelEntreprises, BorderLayout.NORTH);
+		panelEntreprises.setLayout(new BorderLayout(0, 0));
+		
+		listeEntreprises = new JList<String>(remplirListeEntreprises());
+		panelEntreprises.add(listeEntreprises, BorderLayout.CENTER);
+		
+		JPanel panelBases = new JPanel();
+		panelBases.setBorder(new TitledBorder(null, "Bases", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panelEntreprisesBases.add(panelBases, BorderLayout.SOUTH);
+		panelBases.setLayout(new BorderLayout(0, 0));
+		
+		listeBases = new JList<String>(remplirListeBases());
+		panelBases.add(listeBases, BorderLayout.CENTER);
 		
 		JPanel panelLancement = new JPanel();
 		frame.getContentPane().add(panelLancement, BorderLayout.SOUTH);
@@ -163,8 +152,87 @@ public class Ihm {
 		horizontalBoxLancement.add(horizontalGlueLancement);
 		
 		JButton btnLancement = new JButton("Lancement");
+		btnLancement.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				// Initialisation du compteur temps;
+				long debut = System.currentTimeMillis();
+				
+				// Choix des fichiers
+				String cheminListeBases = "./bin/docs/listesBases/" + listeBases.getSelectedValue();
+				String cheminListeEntreprises = "./bin/docs/listesEntreprises/" + listeEntreprises.getSelectedValue();
+				
+				// Lecture des fichiers
+				Lecteur lecteur = new Lecteur(cheminListeBases, cheminListeEntreprises);
+				Model model = new Model();
+				lecteur.lireBases();
+				model.setBases(lecteur.getBases());
+				model.setEntreprises(lecteur.lireEntreprises());
+				model.setEntreprisesBases(lecteur.getEntreprisesBases());
+				
+				// Temps intermédiaire = chargement des fichiers
+				long inter = System.currentTimeMillis();
+				textAreaResultats.append("TEMPS DE CHARGEMENT DES FICHIERS : " + (inter - debut) + " ms\n");
+				
+				if (rdbtnBranchBound.isSelected()) {
+					AlgorithmeBranchAndBound algoBB = new AlgorithmeBranchAndBound(model);
+					// Temps final = temps d'éxecution de l'algorithme
+					long fin = System.currentTimeMillis();
+					textAreaResultats.append("TEMPS D'EXECUTION DE L'ALGORITHME DE BRANCH & BOUND : " + (fin - inter) + " ms\n");
+				}
+				
+				if (rdbtnGlouton.isSelected()) {
+					AlgorithmeGlouton algoGlouton = new AlgorithmeGlouton(model);
+					// Temps final = temps d'éxecution de l'algorithme
+					long fin = System.currentTimeMillis();
+					textAreaResultats.append("TEMPS D'EXECUTION DE L'ALGORITHME GLOUTON : " + (fin - inter) + " ms\n\n");
+					textAreaResultats.append("Meilleur coût : " + algoGlouton.getResultatCout() + "\n\n");
+					for (Entry<String, ArrayList<String>> entry : algoGlouton.getResultatListe().entrySet()) {
+						textAreaResultats.append("\tBase" + entry.getKey() + "\n");
+						for (String entreprise : entry.getValue()) {
+							textAreaResultats.append("\t\t" + entreprise + "\n");
+						}
+					}
+				}
+				
+				
+				
+			}
+		});
 		btnLancement.setHorizontalAlignment(SwingConstants.RIGHT);
 		horizontalBoxLancement.add(btnLancement);
+	}
+	
+	private DefaultListModel<String> remplirListeBases() { 
+		File[] fichiers = null; 
+		File repertoire = new File("bin/docs/listesBases"); 
+		fichiers = repertoire.listFiles();
+		ArrayList<String> listeTemporaire = new ArrayList<String>();
+		DefaultListModel<String> listModel = new DefaultListModel<String>();
+		for (File file : fichiers) {
+			listeTemporaire.add(file.getName());
+		}
+		Collections.sort(listeTemporaire);
+		for (String nomFichier : listeTemporaire) {
+			listModel.addElement(nomFichier);
+		}
+		return listModel;
+	}
+	
+	private DefaultListModel<String> remplirListeEntreprises() { 
+		File[] fichiers = null; 
+		File repertoire = new File("bin/docs/listesEntreprises"); 
+		fichiers = repertoire.listFiles();
+		ArrayList<String> listeTemporaire = new ArrayList<String>();
+		DefaultListModel<String> listModel = new DefaultListModel<String>();
+		for (File file : fichiers) {
+			listeTemporaire.add(file.getName());
+		}
+		Collections.sort(listeTemporaire);
+		for (String nomFichier : listeTemporaire) {
+			listModel.addElement(nomFichier);
+		}
+		return listModel;
 	}
 
 }
